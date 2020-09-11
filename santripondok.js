9288b35cb61d7279374563c20e4b6be827c88270
ninjaa <alkhudrisaid83@gmail.com>
2020-09-11 07:21:04 +0700

    first commit

---
+++ santripondok.js
@@ -0,0 +1,7 @@
+var santriPondok = ["wandi", "aldi", "Ace", "Putra", "hafif", "sofyan", "rahmad"];
+ var banyaksantri = santriPondok.length;
+ document.write("<ol>")
+ for (i = 0; i <ol banyaksantri; i++ ){
+ document.write (<li> + santriPondok + </li>)
+ }
+document.write("</ol")
